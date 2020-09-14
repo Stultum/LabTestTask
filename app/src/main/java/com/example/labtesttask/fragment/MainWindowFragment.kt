2d1b9f9489e5dae5627bfbd65f13f639e40e6e5e
Stultum <tgslaayer@gmail.com>
2020-09-14 11:13:35 +0700

    Added dialog fragment

--- app/src/main/java/com/example/labtesttask/fragment/MainWindowFragment.kt
+++ app/src/main/java/com/example/labtesttask/fragment/MainWindowFragment.kt
@@ -1,34 +1,75 @@
 package com.example.labtesttask.fragment
 
+import android.app.AlertDialog
+import android.content.Context
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 
 import com.example.labtesttask.R
+import com.example.labtesttask.activity.MainActivity
+import com.example.labtesttask.databinding.MainWindowFragmentBinding
+import com.example.labtesttask.repository.ProfileRepository
+import com.example.labtesttask.viewmodel.LoginViewModel
 import com.example.labtesttask.viewmodel.MainWindowViewModel
+import com.example.labtesttask.viewmodelfactory.ViewModelFactory
 
 class MainWindowFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = MainWindowFragment()
-    }
-
+    private lateinit var binding: MainWindowFragmentBinding
     private lateinit var viewModel: MainWindowViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.main_window_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.main_window_fragment, container, false)
+        binding.lifecycleOwner = this
+
+        initViewModel()
+
+        binding.buttonHello.setOnClickListener { showHelloDialog() }
+
+        viewModel.isAccountLeave.observe(this, Observer {
+            if(it){
+                leaveAccount(binding.root)
+            }
+        })
+
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainWindowViewModel::class.java)
-        // TODO: Use the ViewModel
+    private fun initViewModel() {
+        viewModel = ViewModelProvider(
+            this,
+            ViewModelFactory {
+                MainWindowViewModel(
+                    ProfileRepository(
+                        this.context?.getSharedPreferences(
+                            ProfileRepository.PROFILE_PREF,
+                            Context.MODE_PRIVATE
+                        )!!
+                    )
+                )
+            }).get(
+            MainWindowViewModel::class.java
+        )
+        binding.mainWindowViewModel = viewModel
     }
 
+    private fun showHelloDialog(){
+        val dialogBuilder = AlertDialog.Builder(this as Context)
+        dialogBuilder.setView(R.layout.hello_dialog_fragment)
+        dialogBuilder.show()
+    }
+
+    private fun leaveAccount(view: View){
+        view.findNavController().navigate(R.id.action_mainWindowFragment_to_loginFragment)
+    }
 }
--- app/src/main/java/com/example/labtesttask/viewmodel/MainWindowViewModel.kt
+++ app/src/main/java/com/example/labtesttask/viewmodel/MainWindowViewModel.kt
@@ -1,7 +1,21 @@
 package com.example.labtesttask.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.labtesttask.repository.ProfileRepository
+import kotlinx.coroutines.launch
 
-class MainWindowViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainWindowViewModel(private val profileRepository: ProfileRepository) : ViewModel() {
+    private val _isAccountLeave = MutableLiveData<Boolean>()
+    val isAccountLeave: LiveData<Boolean>
+        get() = _isAccountLeave
+
+    fun leaveAccount(){
+        viewModelScope.launch {
+            profileRepository.clearProfile()
+            _isAccountLeave.value = true
+        }
+    }
 }
